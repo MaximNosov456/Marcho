1acf54d8a27496eebd52d8dbb83607ffd6edd64f
MaximNosov456 <ya.typ2013@yandex.ru>
2021-07-10 15:29:38 +0300

    added page blog

--- gulpfile.js
+++ gulpfile.js
@@ -32,6 +32,7 @@ function scripts() {
               'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
               'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
               'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+              'node_modules/slick-carousel/slick/slick.js',
               'app/js/main.js'])
     .pipe(concat('main.min.js'))
     .pipe(uglify())
